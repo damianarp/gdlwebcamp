8c1b6acd888ee241bfc60d64ed8c349609dfc83f
Damian Arp <damianarp@gmail.com>
2020-09-01 21:33:02 -0300

    prueva de modificacion en index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
   <section class="seccion contenedor">
       <h2>La mejor conferencia de diseño web en español</h2>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        Lorem ipsums dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
       </p>
   </section> <!--seccion-->
 
